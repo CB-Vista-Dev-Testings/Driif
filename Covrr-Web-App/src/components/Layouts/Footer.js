0b05e0427a50edb49eaf3d9b978e3deaeb1d23a8
bharath_r <bharathr@cloudbuilders.io>
2025-03-05 13:11:40 +0530

    changes

--- Covrr-Web-App/src/components/Layouts/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="p-4 bg-light-primary dark:bg-dark-primary text-light-text dark:text-dark-text flex justify-center">
-    <p>© 2025 My Website</p>
-    </footer>
-  );
-};
-
-export default Footer;
